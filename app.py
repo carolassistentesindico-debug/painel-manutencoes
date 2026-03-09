549fe64da47c0dc7840a1addb738ccdf8623da6e
Caroline <seuemail@gmail.com>
2026-03-09 20:12:49 -0300

    corrige porta para render

--- app.py
+++ app.py
@@ -1127,4 +1127,5 @@ def api_manutencoes_por_condominio(condominio_id):
     }
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
